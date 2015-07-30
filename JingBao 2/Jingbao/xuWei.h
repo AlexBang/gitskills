175529d9006569e9d8b8a2dae5a3e4c414bba88d
mac <mac@macdeiMac.local>
2015-07-30 17:10:26 +0800

    新建了一个文件

---
+++ JingBao 2/Jingbao/xuWei.h
@@ -0,0 +1,13 @@
+//
+//  xuWei.h
+//  Jingbao
+//
+//  Created by mac on 15/7/30.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface xuWei : NSObject
+
+@end
---
+++ Jingbao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jingbao
+//
+//  Created by mac on 15/7/7.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
